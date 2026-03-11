2a5d06cdd59b59ed9cff1098133385292c5577e6
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-11 19:42:15 +0530

    basic operations like travesing, inserting, deleting and searching element

---
+++ Topics/array/basic_operation.py
@@ -0,0 +1,15 @@
+lst = [10, 20, 30, 50, 70, 50, 40, 20]
+
+for i in lst:
+  print("iteration is ", i)
+  
+lst.append(100)
+lst.append(200)
+print("list after appending 100 and 200 is ", lst)
+
+lst.remove(30)
+print("list after removing 30 is ", lst)
+
+if 500 in lst:
+  print(" 50 is present in list")
+  
