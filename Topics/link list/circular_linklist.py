8975c1039f8331afb0f502d269fe597aa0501d3b
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-20 11:15:24 +0530

    doubly circular ll

--- Topics/link list/circular_linklist.py
+++ Topics/link list/circular_linklist.py
@@ -3,50 +3,63 @@ class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
-    # self.prev = None
-	
-node1 = Node(3)
-node2 = Node(5)
-node3 = Node(1)
-
-node1.next = node2
-node2.next = node3
-node3.next = node1
-
-currentNode = node1
-startNode = node1
-print(currentNode.data, end=" -> ")
-currentNode = currentNode.next
-
-while currentNode != startNode:
-  print(currentNode.data , end=" -> ")
-  currentNode = currentNode.next
-
-
-print("null")
-  
-
-
+    self.prev = None
 	
 # node1 = Node(3)
 # node2 = Node(5)
 # node3 = Node(1)
 
 # node1.next = node2
-# node2.prev = node1
 # node2.next = node3
-# node3.prev = node2
+# node3.next = node1
 
-# print("Traversing forward")
 # currentNode = node1
-# while currentNode:
-#   print(currentNode.data, end =" -> ")
-#   currentNode = currentNode.next
-# print("null")
+# startNode = node1
+# print(" single circular linked list: ")
+# print(currentNode.data, end=" -> ")
+# currentNode = currentNode.next
 
-# print("Traversing backwards")
-# currentNode = node3
-# while currentNode:
-#   print(currentNode.data, end =" -> ")
-#   currentNode = currentNode.prev
+# while currentNode != startNode:
+#   print(currentNode.data , end=" -> ")
+#   currentNode = currentNode.next
+
+
 # print("null")
+  
+
+
+	
+node1 = Node(3)
+node2 = Node(5)
+node3 = Node(1)
+
+node1.next = node2
+node2.prev = node1
+node2.next = node3
+node3.prev = node2
+node3.next = node1
+node1.prev = node3
+
+print("double circular linked list: ")
+
+currentNode = node1
+startNode = node1
+
+print("\n forward traversal: ")
+print(currentNode.data, end=" -> ")
+currentNode = currentNode.next
+while currentNode != startNode:
+  print(currentNode.data, end= " -> ")
+  currentNode = currentNode.next
+print("null")
+
+currentNode = node3
+startNode = node3
+
+print("\n backward traversal: ")
+print(currentNode.data, end= " -> ")
+currentNode = currentNode.prev
+while currentNode != startNode:
+  print(currentNode.data, end =" -> ")
+  currentNode = currentNode.prev
+print("null")
