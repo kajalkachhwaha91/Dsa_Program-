daaf642541e671c92c8470946232f4a751ebedfa
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-20 10:53:30 +0530

    singular circular ll

--- Topics/link list/basic_operation.py
+++ Topics/link list/basic_operation.py
@@ -1,27 +1,25 @@
-# class Node:
-#   def __init__(self, data):
-#     self.data = data
-#     self.next = None
-	
-# node1 = Node(3)
-# node2 = Node(5)
-# node3 = Node(1)
-
-# node1.next = node2
-# node2.next = node3
-
-# currentNode = node1
-# while currentNode:
-#   print(currentNode.data, end=" -> ")
-#   currentNode = currentNode.next
-# print("null")
-  
-
+# single and doubly linked list implementation in python
 class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
     self.prev = None
+	
+node1 = Node(3)
+node2 = Node(5)
+node3 = Node(1)
+
+node1.next = node2
+node2.next = node3
+
+currentNode = node1
+while currentNode:
+  print(currentNode.data, end=" -> ")
+  currentNode = currentNode.next
+print("null")
+  
+
+
 	
 node1 = Node(3)
 node2 = Node(5)
---
+++ Topics/link list/circular_linklist.py
@@ -0,0 +1,52 @@
+# circular linked list implementation in python
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+    # self.prev = None
+	
+node1 = Node(3)
+node2 = Node(5)
+node3 = Node(1)
+
+node1.next = node2
+node2.next = node3
+node3.next = node1
+
+currentNode = node1
+startNode = node1
+print(currentNode.data, end=" -> ")
+currentNode = currentNode.next
+
+while currentNode != startNode:
+  print(currentNode.data , end=" -> ")
+  currentNode = currentNode.next
+
+
+print("null")
+  
+
+
+	
+# node1 = Node(3)
+# node2 = Node(5)
+# node3 = Node(1)
+
+# node1.next = node2
+# node2.prev = node1
+# node2.next = node3
+# node3.prev = node2
+
+# print("Traversing forward")
+# currentNode = node1
+# while currentNode:
+#   print(currentNode.data, end =" -> ")
+#   currentNode = currentNode.next
+# print("null")
+
+# print("Traversing backwards")
+# currentNode = node3
+# while currentNode:
+#   print(currentNode.data, end =" -> ")
+#   currentNode = currentNode.prev
+# print("null")
