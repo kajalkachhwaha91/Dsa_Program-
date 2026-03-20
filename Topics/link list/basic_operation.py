070d88ea4a9087fabd28706eef50d76424438c17
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-20 10:38:44 +0530

    doubly  linked listed

--- Topics/link list/basic_operation.py
+++ Topics/link list/basic_operation.py
@@ -1,18 +1,47 @@
+# class Node:
+#   def __init__(self, data):
+#     self.data = data
+#     self.next = None
+	
+# node1 = Node(3)
+# node2 = Node(5)
+# node3 = Node(1)
+
+# node1.next = node2
+# node2.next = node3
+
+# currentNode = node1
+# while currentNode:
+#   print(currentNode.data, end=" -> ")
+#   currentNode = currentNode.next
+# print("null")
+  
+
 class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
+    self.prev = None
 	
 node1 = Node(3)
 node2 = Node(5)
 node3 = Node(1)
 
 node1.next = node2
+node2.prev = node1
 node2.next = node3
+node3.prev = node2
 
+print("Traversing forward")
 currentNode = node1
 while currentNode:
-  print(currentNode.data, end=" -> ")
+  print(currentNode.data, end =" -> ")
   currentNode = currentNode.next
 print("null")
-  
+
+print("Traversing backwards")
+currentNode = node3
+while currentNode:
+  print(currentNode.data, end =" -> ")
+  currentNode = currentNode.prev
+print("null")
