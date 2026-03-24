a56c316ec25e0215f95f7e79ff1cac5b5cc9ccdb
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-24 10:34:23 +0530

    move zero to last

---
+++ Topics/array/move_zero_to _end.py
@@ -0,0 +1,18 @@
+arr = [ 2, 4, 0, 7, 8,0, 4, 0, 2]
+pos = 0
+
+for i in range (len(arr)):
+  if arr[i] != 0:
+    arr[pos] = arr[i]
+    pos+=1
+	
+for i in range(pos, len(arr)):
+  arr[i] = 0
+	
+	
+print(arr)
+
+
+
+
+
