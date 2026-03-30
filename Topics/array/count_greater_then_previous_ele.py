3bb7fae3a42f947528c8705ea53f322113ccea07
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-30 10:36:39 +0530

    improvemnet

--- Topics/array/count_greater_then_previous_ele.py
+++ Topics/array/count_greater_then_previous_ele.py
@@ -8,4 +8,7 @@ for i in range(1, n):
         count += 1
     max_so_far = max(max_so_far, arr[i])
 
-print(count)
+
+# import mathGiven an integer array Arr of size N the task is to find the count of elements whose value is greater than all of its prior elements.
+
+# Note : 1st element of the array should be considered in the count of the result.
